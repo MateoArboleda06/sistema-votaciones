738881b3433dcb5742da1100071be8d902223972
mateo <teo.arb03@gmail.com>
2025-05-27 06:22:27 -0500

    Enrutamiento del backend API con proteccion de rutas tanto por autentificacion como por roles

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\QuestionController;
+use App\Http\Controllers\API\ResultController;
+use App\Http\Controllers\API\VoteController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
@@ -12,4 +15,13 @@ Route::post('/login', LoginController::class);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', UserController::class);
     Route::post('/logout', LogoutController::class);
+
+    Route::middleware(['role:administrador'])->group(function () {
+        Route::post('/questions', [QuestionController::class, 'store']);
+        Route::get('/results/{question}', [ResultController::class, 'show']);
+    });
+
+    Route::get('/questions', [QuestionController::class, 'index']);
+    Route::get('/questions/{question}', [QuestionController::class, 'show']);
+    Route::post('/votes', [VoteController::class, 'store']);
 });
